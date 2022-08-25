1c7a8c5231dadb88bda914da6fa7b747f239bbaa
aqilusman45 <aqilusman45@gmail.com>
2022-08-25 11:19:35 -0500

    Web3 - transaction listener - add withdraw function

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ const FUNDME_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fundButton");
+const getBalance = document.getElementById("getBalance");
+const withdrawButton = document.getElementById("withdrawButton");
 
 connectButton.addEventListener("click", connect);
 fundButton.addEventListener("click", fund);
+getBalance.addEventListener("click", getContractBalance);
+withdrawButton.addEventListener("click", withdraw);
 
 (async () => {
   const accounts = await ethereum.request({ method: "eth_accounts" });
@@ -32,16 +36,48 @@ async function connect() {
   }
 }
 
+async function withdraw() {
+  if (isEthereumLoaded) {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const fundMe = new ethers.Contract(FUNDME_ADDRESS, FUNDME_ABI, signer);
+    const transactionResponse = await fundMe.withdraw();
+    await listenForTransactionMine(transactionResponse, provider);
+    console.log("Done");
+  }
+}
+async function getContractBalance() {
+  if (isEthereumLoaded) {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const res = await provider.getBalance(FUNDME_ADDRESS);
+    console.log(ethers.utils.formatEther(res));
+  }
+}
+
 async function fund() {
   if (isEthereumLoaded) {
-    const amount = "1";
+    const ethAmount = document.getElementById("ethAmount").value;
+    if (!ethAmount) {
+      alert("Cannot be empty!");
+      return;
+    }
     const provider = new ethers.providers.Web3Provider(ethereum);
     const signer = provider.getSigner();
     const fundMe = new ethers.Contract(FUNDME_ADDRESS, FUNDME_ABI, signer);
     const transactionResponse = await fundMe.fund({
-      value: ethers.utils.parseEther(amount),
+      value: ethers.utils.parseEther(ethAmount),
     });
-    const txReceipt = await transactionResponse.wait(1);
-    console.log(txReceipt);
+    await listenForTransactionMine(transactionResponse, provider);
+    console.log("Done");
   }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}`);
+  return new Promise((res, rej) => {
+    provider.once(transactionResponse.hash, (txReceipt) => {
+      console.log("Transaction completed", txReceipt.confirmations);
+      res();
+    });
+  });
 }
